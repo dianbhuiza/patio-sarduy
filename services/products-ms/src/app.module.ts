9dc8faa7a3de13a1edbaa7f80961b7044e2312b1
dianbhuiza <diandev03@gmail.com>
2026-01-02 16:55:02 -0500

    feat: configure TypeORM with PostgreSQL and integrate ProductsModule

--- services/products-ms/src/app.module.ts
+++ services/products-ms/src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ProductsModule } from './modules/products/products.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { env } from './config/env';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: env.DB_HOST,
+      port: env.DB_PORT,
+      username: env.DB_USER,
+      password: env.DB_PASSWORD,
+      database: env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: env.NODE_ENV !== 'production',
+    }),
+    ProductsModule
+  ],
 })
 export class AppModule {}
